a20f2f33102b534e4da07682664fc2819c850926
Tomás Pérez <t@0x101.com>
2018-04-01 20:43:45 +0200

    Implementing company specialties resolution.

--- core/linkedin.js
+++ core/linkedin.js
@@ -2,25 +2,21 @@
  * Layer on top of the `node-linkedin` library used to enrich the information obtained from the
  * Github API.
  */
-const Linkedin = require('node-linkedin')(config.clientId, config.clientSecret, config.redirectUri);
+const linkedinConfig = require('../config/my-linkedin-app.json');
+const Linkedin = require('node-linkedin')(linkedinConfig.clientId, linkedinConfig.clientSecret, linkedinConfig.redirectUri);
 
-const linkedinConfig = require('./config/my-linkedin-app.json');
+const linkedinApiClient = Linkedin.init(linkedinConfig.accessToken);
 
-const linkedinApiClient = Linkedin.init(config.accessToken);
+module.exports = (name) => {
+  return new Promise(resolve => {
+    linkedinApiClient.companies_search.name(name, 1, (err, result) => {
+      console.log('querying for ' + name);
+      let company = null;
+      if (result && result.companies && result.companies.values) {
+        company = result.companies.values[0];
+      }
 
-let count = 0;
-linkedinApiClient.companies_search.name(company.name, 1, function(err, result) {
-  console.log('Processing ' + company.name);
-  console.log(result);
-  if (result && result.companies && result.companies.values) {
-    result.companies.values.forEach((c) => {
-      writer.write({ name: company.name, technology: c.specialites.values.join(' ')});
+      resolve(company);
     });
-
-    count++;
-    if (count > 2) {
-      writer.end();
-      process.exit();
-    }
-  }
-});
+  });
+};
